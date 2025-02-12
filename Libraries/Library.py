84b5146ee2890c0efe83265d6a9d2964272e575f
Pekka Klärck <peke@iki.fi>
2025-02-12 12:20:24 +0200

    More examples

--- Libraries/Library.py
+++ Libraries/Library.py
@@ -1,10 +1,20 @@
 from enum import auto, Enum
-from datetime import timedelta
+from datetime import date, datetime, timedelta
 from decimal import Decimal
 from pathlib import Path
 from typing import Literal
 
 from robot.api.deco import keyword, library
+from robot.api.exceptions import ContinuableFailure, SkipExecution
+
+
+class EuroDate(date):
+    """Date represented as dd.mm.yyyy."""
+
+    @classmethod
+    def from_string(cls, day: str) -> 'EuroDate':
+        dt = datetime.strptime(day, '%d.%m.%Y')
+        return cls(dt.year, dt.month, dt.day)
 
 
 class Direction(Enum):
@@ -13,8 +23,18 @@ class Direction(Enum):
     RIGHT = auto()
 
 
-@library
+@library(scope='SUITE',
+         converters={EuroDate: EuroDate.from_string})
 class Library:
+
+    def __init__(self, state='INITIAL'):
+        self.state = state
+
+    @keyword
+    def set_state(self, state: str):
+        print(f'Old state was {self.state}.')
+        self.state = state
+        print(f'New state is {self.state}.')
 
     @keyword
     def conversion(self, number: Decimal, path: Path,
@@ -33,3 +53,34 @@ class Library:
             print('Turning left')
         else:
             print('Turning right')
+
+    @keyword
+    def birthday(self, day: EuroDate):
+        today = EuroDate.today()
+        day = day.replace(year=today.year)
+        difference = (day - today).days
+        print(f"It's {difference} days to your birthday!")
+
+    @keyword(name='🤖')
+    def robot(self):
+        print('🤖')
+
+    @keyword('This is a "${kind}" example')
+    def example(self, kind):
+        print(kind)
+
+    @keyword('Number of ${animal} is')
+    def number_of_animals_is(self, animal, count: int):
+        print(animal, count)
+
+    @keyword
+    def skip_me(self):
+        raise SkipExecution('Some good reason...')
+
+    @keyword
+    def continuable_failure(self, message):
+        raise ContinuableFailure(message)
+
+    @keyword
+    def html_error(self):
+        raise AssertionError('*HTML* Something <b>bad</b> happened!')
--- Libraries/tests.robot
+++ Libraries/tests.robot
@@ -1,5 +1,5 @@
 *** Settings ***
-Library            Library.py
+Library            Library.py    ORIGINAL
 
 *** Test Cases ***
 Argument conversion
@@ -17,3 +17,36 @@ Restricted values using Enum
     Turn    LEFT
     Turn    right
     Turn    wrong
+
+Custom converts
+    Birthday    6.4.1975
+
+Library state 1
+    Set state    NEW
+    Set state    NEWER
+
+Library state 2
+    Set state    NEWEST
+
+Custom keyword name
+    🤖
+
+Embedded arguments
+    This is a "good" example
+    This is a "bad" example
+    This is a "ugly" example
+
+Mixed arguments
+    Number of horses is    6
+    Number of dogs is    2
+
+Skipping
+    Skip me
+
+Continuing on failure
+    Continuable failure    first
+    Continuable failure    second
+    Continuable failure    third
+
+HTML error messages
+    HTML error
