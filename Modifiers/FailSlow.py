16047a72a45ce47df3b5a1f1d6aa2d0dd8f4d37b
Pekka Klärck <peke@iki.fi>
2025-02-28 14:22:02 +0200

    Add usage. Change threshold from float to timedelta.

--- Modifiers/FailSlow.py
+++ Modifiers/FailSlow.py
@@ -1,4 +1,24 @@
+#!/usr/bin/env python
+
+"""Utility to mark test that take too long time failed.
+
+Can be used either as a separate script or as a pre-Rebot modifier as part of
+test execution or when using Rebot.
+
+Usage as a script:
+    python FailSlow.py output_file max_seconds
+
+Usage as a modifier:
+    robot --prerebotmodifier FailSlow.py:max_seconds path/to/tests.robot
+    rebot --prerebotmodifier FailSlow.py:max_seconds path/to/output.xml
+
+Notice that when used as part of test execution, test statuses reported on
+the console and in the output file are not affected. Changes are seen only in
+the generated log and report files.
+"""
+
 import sys
+from datetime import timedelta
 
 from robot.api import ExecutionResult, SuiteVisitor
 from robot.result import TestCase
@@ -6,8 +26,8 @@ from robot.result import TestCase
 
 class FailSlow(SuiteVisitor):
 
-    def __init__(self, threshold: float):
-        self.threshold = threshold
+    def __init__(self, threshold: timedelta):
+        self.threshold = threshold.total_seconds()
 
     def start_test(self, test: TestCase):
         elapsed = test.elapsed_time.total_seconds()
@@ -19,6 +39,6 @@ class FailSlow(SuiteVisitor):
 if __name__ == '__main__':
     threshold, path = sys.argv[1:]
     result = ExecutionResult(path)
-    visitor = FailSlow(float(threshold))
+    visitor = FailSlow(timedelta(seconds=float(threshold)))
     result.suite.visit(visitor)
     result.save()
